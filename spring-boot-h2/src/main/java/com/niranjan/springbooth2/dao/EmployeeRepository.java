48daceca3b081f8a9c4da8d03445f9a5347540b5
Niranjan JD <jdniranjanchowdary39@gmail.com>
2024-03-04 21:10:35 +0530

    First Commit

--- spring-boot-h2/src/main/java/com/niranjan/springbooth2/dao/EmployeeRepository.java
+++ spring-boot-h2/src/main/java/com/niranjan/springbooth2/dao/EmployeeRepository.java
@@ -2,7 +2,6 @@ package com.niranjan.springbooth2.dao;
 
 import com.niranjan.springbooth2.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
